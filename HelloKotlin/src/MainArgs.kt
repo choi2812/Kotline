afb42dba2f88372ac5a568be89b5167f7c25bded
j.cui <j.cui2812@gmail.com>
2019-12-18 00:31:13 +0900

    01장 코틀린 시작하기

---
+++ HelloKotlin/src/MainArgs.kt
@@ -0,0 +1,10 @@
+//main함수의 매개변수 사용
+fun main(args: Array<String>) {
+    println(args[0])
+    println(args[1])
+    println(args[2])
+}
+//Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: 0 -> 명령행에서 아무 인자가 설정되어 있지 않음
+//Run ->Edit Configurations->Program arguments
+//hello 안녕 こんにちは
+//명령형 인자 나열
